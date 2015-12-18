27021a966ec5f41536744d62a0d464e2671841d4
Doug Sale <dougsale@gmail.com>
2015-12-18 00:10:35 +0000

    BoundingBox - added consturctor parameter validation - added some documentation
    BoundingBoxTest
    - unit test coverage

--- chicagotrafficcameras-domain/src/main/java/net/dougsale/chicagotrafficcameras/camerafilters/BoundingBox.java
+++ chicagotrafficcameras-domain/src/main/java/net/dougsale/chicagotrafficcameras/camerafilters/BoundingBox.java
@@ -1,5 +1,8 @@
 package net.dougsale.chicagotrafficcameras.camerafilters;
 
+import static org.apache.commons.lang3.Validate.isTrue;
+import static org.apache.commons.lang3.Validate.notNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,14 +20,27 @@ public class BoundingBox implements CameraFilter {
 	private double minLng;
 	private double maxLng;						
 	
-	public BoundingBox(Step step, double boxPadding) {
+	/**
+	 * BoundingBox filters cameras that aren't located in a box
+	 * defined the start and the stop of the step parameter and
+	 * a padding value applied equally in all 4 cardinal directions.
+	 * Note that the padding is in degrees and thus isn't equal in absolute
+	 * distance between latitude and longitude.  Over small padding values
+	 * this is considered negligible.
+	 * @param step the step of the route directions
+	 * @param padding allowed variance, in degrees
+	 */
+	public BoundingBox(Step step, double padding) {
+		notNull(step, "invalid step: null");
+		isTrue(padding >= 0.0, "invalid padding: must be non-negative");
+		
 		Location start = step.start;
 		Location end = step.end;
 		
-		minLat = Math.min(start.latitude, end.latitude) - boxPadding;
-		maxLat = Math.max(start.latitude, end.latitude) + boxPadding;
-		minLng = Math.min(start.longitude, end.longitude) - boxPadding;
-		maxLng = Math.max(start.longitude, end.longitude) + boxPadding;						
+		minLat = Math.min(start.latitude, end.latitude) - padding;
+		maxLat = Math.max(start.latitude, end.latitude) + padding;
+		minLng = Math.min(start.longitude, end.longitude) - padding;
+		maxLng = Math.max(start.longitude, end.longitude) + padding;						
 	}
 	
 	public boolean inBounds(Camera camera) {
---
+++ chicagotrafficcameras-domain/src/test/java/net/dougsale/chicagotrafficcameras/camerafilters/BoundingBoxTest.java
@@ -0,0 +1,161 @@
+package net.dougsale.chicagotrafficcameras.camerafilters;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+
+import java.util.Arrays;
+import java.util.EnumSet;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+import net.dougsale.chicagotrafficcameras.Directions;
+import net.dougsale.chicagotrafficcameras.Directions.Step;
+import net.dougsale.chicagotrafficcameras.domain.Approach;
+import net.dougsale.chicagotrafficcameras.domain.Location;
+import net.dougsale.chicagotrafficcameras.domain.RedLightCamera;
+import net.dougsale.chicagotrafficcameras.domain.SpeedCamera;
+
+public class BoundingBoxTest {
+
+	
+	@Test(expected=NullPointerException.class)
+	public void testBoundingBoxNullStep() {
+		new BoundingBox(null, 0.01);
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testBoundingBoxInvalidPadding() {
+		new BoundingBox(new Directions.Step("instructions", mock(Location.class), mock(Location.class)), -0.01);
+	}
+
+	Location cameraLocation = new Location(41.867401834997956, -87.72508368792016);
+	SpeedCamera spCamera = new SpeedCamera("address", cameraLocation, EnumSet.of(Approach.WESTBOUND));
+	RedLightCamera rlCamera = new RedLightCamera(
+		new HashSet<>(Arrays.asList("street1", "street2")), cameraLocation, EnumSet.of(Approach.WESTBOUND));
+	
+	@Test
+	public void testInBounds() {
+		double delta = 0.0011;
+		double padding = 0.001;
+		{ // box (excluding padding) is a point, the same as the camera
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(true));
+			assertThat(box.inBounds(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted south of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude - padding, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude - padding, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(true));
+			assertThat(box.inBounds(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted east of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude - padding);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude - padding);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(true));
+			assertThat(box.inBounds(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted south and east of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude - padding, cameraLocation.longitude - padding);
+			Location stepEnd = new Location(cameraLocation.latitude - padding, cameraLocation.longitude - padding);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(true));
+			assertThat(box.inBounds(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted north of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude + delta, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude + delta, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(false));
+			assertThat(box.inBounds(rlCamera), is(false));
+		}
+		{ // box (excluding padding) is a point shifted west of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude + delta);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude + delta);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(false));
+			assertThat(box.inBounds(rlCamera), is(false));
+		}
+		{ // box (excluding padding) is a point shifted north and west of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude + delta, cameraLocation.longitude + delta);
+			Location stepEnd = new Location(cameraLocation.latitude + delta, cameraLocation.longitude + delta);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.inBounds(spCamera), is(false));
+			assertThat(box.inBounds(rlCamera), is(false));
+		}
+	}
+
+	@Test
+	public void testAccept() {
+		double delta = 0.0011;
+		double padding = 0.001;
+		{ // box (excluding padding) is a point, the same as the camera
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(true));
+			assertThat(box.accept(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted south of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude - padding, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude - padding, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(true));
+			assertThat(box.accept(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted east of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude - padding);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude - padding);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(true));
+			assertThat(box.accept(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted south and east of the camera, but within the padding value
+			Location stepStart = new Location(cameraLocation.latitude - padding, cameraLocation.longitude - padding);
+			Location stepEnd = new Location(cameraLocation.latitude - padding, cameraLocation.longitude - padding);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(true));
+			assertThat(box.accept(rlCamera), is(true));
+		}
+		{ // box (excluding padding) is a point shifted north of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude + delta, cameraLocation.longitude);
+			Location stepEnd = new Location(cameraLocation.latitude + delta, cameraLocation.longitude);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(false));
+			assertThat(box.accept(rlCamera), is(false));
+		}
+		{ // box (excluding padding) is a point shifted west of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude, cameraLocation.longitude + delta);
+			Location stepEnd = new Location(cameraLocation.latitude, cameraLocation.longitude + delta);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(false));
+			assertThat(box.accept(rlCamera), is(false));
+		}
+		{ // box (excluding padding) is a point shifted north and west of the camera, not within the padding value
+			Location stepStart = new Location(cameraLocation.latitude + delta, cameraLocation.longitude + delta);
+			Location stepEnd = new Location(cameraLocation.latitude + delta, cameraLocation.longitude + delta);
+			Step step = new Step("instructions", stepStart, stepEnd);
+			BoundingBox box = new BoundingBox(step, padding);
+			assertThat(box.accept(spCamera), is(false));
+			assertThat(box.accept(rlCamera), is(false));
+		}
+	}
+}
