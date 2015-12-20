96fae039360f3809844d31509cd7161713c42c05
Doug Sale <dougsale@gmail.com>
2015-12-20 02:37:00 +0000

    CameraLocator: - cleanup
    CameraLocatorTest:
    - unit test coverage
    - actually, this is more like an integration test

--- chicagotrafficcameras-domain/src/main/java/net/dougsale/chicagotrafficcameras/CameraLocator.java
+++ chicagotrafficcameras-domain/src/main/java/net/dougsale/chicagotrafficcameras/CameraLocator.java
@@ -4,8 +4,7 @@
  */
 package net.dougsale.chicagotrafficcameras;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import static org.apache.commons.lang3.Validate.notNull;
 
 import net.dougsale.chicagotrafficcameras.Directions.Step;
 import net.dougsale.chicagotrafficcameras.camerafilters.BoundingBox;
@@ -20,34 +19,21 @@ import net.dougsale.chicagotrafficcameras.domain.SpeedCamera;
  */
 public class CameraLocator {
 
-	private static final Logger logger = LoggerFactory.getLogger(CameraLocator.class);
-	
-	// NOTE
-	// it is possible that google and the city of chicago
-	// use slightly different encoding standards with
-	// regards to location (i.e., google's start and end
-	// coordinates for a step in relation to chicago's
-	// coordinates for its traffic cameras)
-	
 	// pad bounding box as cameras may lay outside the box
-	// defined by the route
+	// defined by the route (especially when the route is
+	// almost strictly horizontal or vertical)
 	// TODO determine an appropriate value
 	static final double BOX_PADDING = 0.0003;
 	
-	// for cameras within the bounding box, ensure
-	// they are within a certain distance threshold
-	// from the line segment defined by the start
-	// and end locations
-	// TODO determine an appropriate value
-	static final double DISTANCE_THRESHOLD = 0.0003;
-
 	private Cameras allCameras;
 	
 	public CameraLocator(Cameras cameras) {
+		notNull(cameras, "Invalid parameter: cameras=" + cameras);
 		this.allCameras = cameras;
 	}
 	
 	public Cameras locate(Directions directions) {
+		notNull(directions, "invalid parameter: directions=" + directions);
 		
 		StreetMatcherFactory smFactory = new StreetMatcherFactory(directions);
 		
@@ -56,10 +42,12 @@ public class CameraLocator {
 		for (Step step : directions.steps) {
 
 			BoundingBox box = new BoundingBox(step, BOX_PADDING);
-//			StepSegment segment = new StepSegment(step, DISTANCE_THRESHOLD);
 			StreetMatcher street = smFactory.get(step);
 
-			// TODO this would be a great place to implement stream processing
+			// TODO the following would be a good candidate for stream processing
+
+			// TODO determine smallest range of box dimensions (either lat or long) and
+			// then get a navigable set from cameras and only iterate over that subrange
 			
 			for (RedLightCamera camera : allCameras.get(RedLightCamera.class)) {
 				if (box.accept(camera)  && street.accept(camera)) {
@@ -76,5 +64,4 @@ public class CameraLocator {
 		
 		return foundCameras;
 	}
-
 }
--- chicagotrafficcameras-domain/src/test/java/net/dougsale/chicagotrafficcameras/CameraLocatorTest.java
+++ chicagotrafficcameras-domain/src/test/java/net/dougsale/chicagotrafficcameras/CameraLocatorTest.java
@@ -5,16 +5,14 @@
 package net.dougsale.chicagotrafficcameras;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.List;
+import java.util.NavigableSet;
 import java.util.Set;
 
 import org.junit.BeforeClass;
@@ -22,106 +20,82 @@ import org.junit.Test;
 
 import net.dougsale.chicagotrafficcameras.domain.Approach;
 import net.dougsale.chicagotrafficcameras.domain.Location;
+import net.dougsale.chicagotrafficcameras.domain.RedLightCamera;
 import net.dougsale.chicagotrafficcameras.domain.SpeedCamera;
 import net.dougsale.chicagotrafficcameras.repository.CamerasRepository;
 
 public class CameraLocatorTest {
 
-	CameraLocator locator;
+	static Cameras cameras = null;
 	
-//	@BeforeClass
-//	void init() throws ClassNotFoundException, IOException {
-//	}
-//	
+	@BeforeClass 
+	public static void init() throws ClassNotFoundException, IOException {
+		cameras = new CamerasRepository("Cameras.ser").getCameras();
+	} 
+	
+	@Test(expected=NullPointerException.class)
+	public void testCameraLocatorCamerasNull() {
+		new CameraLocator(null);
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void testLocateDirectionsNull() {
+		new CameraLocator(cameras).locate(null);
+	}
+	
 	@Test
-	public void testLocateCamerasOnDiagonalRoute() throws ClassNotFoundException, IOException {
-		// shows the limitations of locating using bounding box only:
-		// box is big, leads to false positives
+	public void testLocateCameras_615_N_Ogden_Ave_3315_W_Ogden_Ave() {
+		// route with large diagonal steps, i.e., large bounding boxes; so taxes street matcher filtering
 		
-		// trying to correct by calculating minimum distance from camera to line segment
-		// originally, 5 cameras show up with bounding box, only 2 are valid:  those with addresses on Ogden
-		// one has the smallest distance, the other the largest
-		// i'm not sure if this is an artifact of the calculation, i've tried scaling the arguments and the result
-		// as you can see the results are the same for multiple scaling values
-		// i think i should try the calculations - a little test program - using BigDecimal with the original text
-		// values from the CSV (Cameras, Chicago) and the .js (start and end line segment, google)
-		// if that still gives erroneous results, it's and issue with the coding of the cameras, likely (thx, Chicago)
-		// in such a case, only matching by street name will work (i'm afraid that has it's own pitfalls...)
-		// street name is easy to implement, though (just concerned about multiple names for streets: Cicero/Mandela, North Ave, rte 64, etc)
-		
-//		UNSCALED: 0.0015802  *** net.dougsale.chicagotrafficcameras.domain.RedLightCamera@446cdf90[location=net.dougsale.chicagotrafficcameras.domain.Location@799f7e29[latitude=41.881454,longitude=-87.666802],approaches=[NORTHBOUND, WESTBOUND],intersection=[Madison, Ashland]]
-//		SCALED:   0.0015802  *** net.dougsale.chicagotrafficcameras.domain.RedLightCamera@446cdf90[location=net.dougsale.chicagotrafficcameras.domain.Location@799f7e29[latitude=41.881454,longitude=-87.666802],approaches=[NORTHBOUND, WESTBOUND],intersection=[Madison, Ashland]]
-//		UNSCALED: 0.0022642  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@5c29bfd[location=net.dougsale.chicagotrafficcameras.domain.Location@7aec35a[latitude=41.883191521391076,longitude=-87.66411469955516],approaches=[NORTHBOUND, SOUTHBOUND],address=115 N Ogden]
-//		SCALED:   0.0022642  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@5c29bfd[location=net.dougsale.chicagotrafficcameras.domain.Location@7aec35a[latitude=41.883191521391076,longitude=-87.66411469955516],approaches=[NORTHBOUND, SOUTHBOUND],address=115 N Ogden]
-//		UNSCALED: 0.0007455  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@42110406[location=net.dougsale.chicagotrafficcameras.domain.Location@531d72ca[latitude=41.88451419700416,longitude=-87.66704209415728],approaches=[SOUTHBOUND],address=140 N Ashland]
-//		SCALED:   0.0007455  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@42110406[location=net.dougsale.chicagotrafficcameras.domain.Location@531d72ca[latitude=41.88451419700416,longitude=-87.66704209415728],approaches=[SOUTHBOUND],address=140 N Ashland]
-//		UNSCALED: 0.0005393  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@22d8cfe0[location=net.dougsale.chicagotrafficcameras.domain.Location@579bb367[latitude=41.88451740794813,longitude=-87.66674834697315],approaches=[NORTHBOUND],address=141 N Ashland]
-//		SCALED:   0.0005393  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@22d8cfe0[location=net.dougsale.chicagotrafficcameras.domain.Location@579bb367[latitude=41.88451740794813,longitude=-87.66674834697315],approaches=[NORTHBOUND],address=141 N Ashland]
-//		UNSCALED: 0.0000161  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@1de0aca6[location=net.dougsale.chicagotrafficcameras.domain.Location@255316f2[latitude=41.86040786445197,longitude=-87.69867209877323],approaches=[EASTBOUND, WESTBOUND],address=2900 W Ogden Ave]
-//		SCALED:   0.0000161  *** net.dougsale.chicagotrafficcameras.domain.SpeedCamera@1de0aca6[location=net.dougsale.chicagotrafficcameras.domain.Location@255316f2[latitude=41.86040786445197,longitude=-87.69867209877323],approaches=[EASTBOUND, WESTBOUND],address=2900 W Ogden Ave]
+		String json = "{\"startAddress\":\"615 N Ogden Ave, Chicago, IL 60642, USA\",\"endAddress\":\"3315 W Ogden Ave, Chicago, IL 60623, USA\",\"steps\":[{\"instructions\":\"Head <b>northeast</b> on <b>N Ogden Ave</b> toward <b>W Erie St</b>\",\"start\":{\"latitude\":41.8929664,\"longitude\":-87.65731360000001},\"end\":{\"latitude\":41.8949373,\"longitude\":-87.65565119999997}},{\"instructions\":\"Make a <b>U-turn</b> at <b>W Superior St</b>\",\"start\":{\"latitude\":41.8949373,\"longitude\":-87.65565119999997},\"end\":{\"latitude\":41.8666981,\"longitude\":-87.6836672}},{\"instructions\":\"Keep <b>left</b> to continue on <b>Historic U.S. 66 W</b>/<b>W Ogden Ave</b>\",\"start\":{\"latitude\":41.8666981,\"longitude\":-87.6836672},\"end\":{\"latitude\":41.8571103,\"longitude\":-87.7078085}},{\"instructions\":\"Keep <b>right</b> to continue on <b>W Ogden Ave</b>\",\"start\":{\"latitude\":41.8571103,\"longitude\":-87.7078085},\"end\":{\"latitude\":41.8567775,\"longitude\":-87.7090442}},{\"instructions\":\"Make a <b>U-turn</b> at <b>S Christiana Ave</b><div style=\\\"font-size:0.9em\\\">Destination will be on the right</div>\",\"start\":{\"latitude\":41.8567775,\"longitude\":-87.7090442},\"end\":{\"latitude\":41.8566225,\"longitude\":-87.70858179999999}}]}";
+		Directions directions = DirectionsTestUtility.getDirections(json);
 
-
-		locator = new CameraLocator(new CamerasRepository("Cameras.ser").getCameras());
-
-		String startAddress = "615 N Ogden Ave, Chicago, IL 60642, USA";
-		String endAddress = "3315 W Ogden Ave, Chicago, IL 60623, USA";
-		List<Directions.Step> steps =
-			Arrays.asList(
-				new Directions.Step(
-					"Head <b>northeast</b> on <b>N Ogden Ave</b> toward <b>W Erie St</b>",
-					new Location(41.8929664, -87.65731360000001),
-					new Location(41.8949373, -87.65565119999997)
-				),
-				new Directions.Step(
-					"Make a <b>U-turn</b> at <b>W Superior St</b>",
-					new Location(41.8949373, -87.65565119999997),
-					new Location(41.8666981, -87.6836672)
-				),
-				new Directions.Step(
-					"Keep <b>left</b> to continue on <b>Historic U.S. 66 W</b>/<b>W Ogden Ave</b>",
-					new Location(41.8666981, -87.6836672),
-					new Location(41.8630448, -87.69086219999997)
-				),
-				new Directions.Step(
-					"Keep <b>right</b> to continue on <b>W Ogden Ave</b>",
-					new Location(41.8630448, -87.69086219999997),
-					new Location(41.8618937, -87.6944863)
-				),
-				new Directions.Step(
-					"Keep <b>left</b> to continue on <b>Historic U.S. 66 W</b>/<b>W Ogden Ave</b>",
-					new Location(41.8618937, -87.6944863),
-					new Location(41.8571103, -87.7078085)
-				),
-				new Directions.Step(
-					"Keep <b>right</b> to continue on <b>W Ogden Ave</b>",
-					new Location(41.8571103, -87.7078085),
-					new Location(41.8567775, -87.7090442)
-				),
-				new Directions.Step(
-					"Make a <b>U-turn</b> at <b>S Christiana Ave</b><div style=\"font-size:0.9em\">Destination will be on the right</div>",
-					new Location(41.8567775, -87.7090442),
-					new Location(41.8566225, -87.70858179999999)
-				)
-			);
-		
-		Directions directions = new Directions(startAddress, endAddress, steps);
+		CameraLocator locator = new CameraLocator(cameras);
 		Cameras located = locator.locate(directions);
 		
-		// should be no red light cameras.  with only bounding box filter these also match: Madison & Ashland red light camera
-		assertThat(located.getTypes(), equalTo(Collections.singleton(SpeedCamera.class)));
+		assertThat(located.get(RedLightCamera.class).size(), equalTo(0));
+		assertThat(located.get(SpeedCamera.class).size(), equalTo(2));
 		
-		Set<SpeedCamera> cameras = located.get(SpeedCamera.class);
-		// should be only 2 speed cameras.  with only bounding box filter these also match: 140 & 141 N Ashland
-		assertThat(cameras.size(), equalTo(2));
-
+		NavigableSet<SpeedCamera> cameras = located.get(SpeedCamera.class, Cameras.Sorted.BY_LATITUDE);
 		Iterator<SpeedCamera> iter = cameras.iterator();
-		SpeedCamera camera1 = iter.next();
-		SpeedCamera camera2 = iter.next();
+		assertThat(iter.next(), equalTo(
+				new SpeedCamera("2900 W Ogden Ave", new Location(41.86040786445197, -87.69867209877323),
+						EnumSet.of(Approach.EASTBOUND, Approach.WESTBOUND))));
+		assertThat(iter.next(), equalTo(
+				new SpeedCamera("115 N Ogden", new Location(41.883191521391076, -87.66411469955516),
+						EnumSet.of(Approach.NORTHBOUND, Approach.SOUTHBOUND))));
+	}
+	
+	@Test
+	public void testLocateCameras_201_N_Columbus_Dr_400_E_Illinois_St() {
+		// route with large diagonal steps, i.e., large bounding boxes; so taxes street matcher filtering
 		
-		assertThat(
-			(camera1.address.equals("2900 W Ogden Ave") && camera2.address.equals("115 N Ogden")) ||
-			(camera1.address.equals("115 N Ogden") && camera2.address.equals("2900 W Ogden Ave")),
-			is(true)
-		);
+		String json = "{\"startAddress\":\"201 N Columbus Dr, Chicago, IL 60611, USA\",\"endAddress\":\"400 E Illinois St, Chicago, IL 60611, USA\",\"steps\":[{\"instructions\":\"Head <b>south</b> on <b>N Columbus Dr</b> toward <b>E Lower Wacker Dr</b>\",\"start\":{\"latitude\":41.8880992,\"longitude\":-87.6207005},\"end\":{\"latitude\":41.88776319999999,\"longitude\":-87.62070829999999}},{\"instructions\":\"Make a <b>U-turn</b> at <b>E Lower Wacker Dr</b>\",\"start\":{\"latitude\":41.88776319999999,\"longitude\":-87.62070829999999},\"end\":{\"latitude\":41.8910117,\"longitude\":-87.62012340000001}},{\"instructions\":\"Turn <b>right</b> onto <b>E Illinois St</b><div style=\\\"font-size:0.9em\\\">Destination will be on the left</div>\",\"start\":{\"latitude\":41.8910117,\"longitude\":-87.62012340000001},\"end\":{\"latitude\":41.8910524,\"longitude\":-87.6178458}}]}";
+		Directions directions = DirectionsTestUtility.getDirections(json);
+
+		CameraLocator locator = new CameraLocator(cameras);
+		Cameras located = locator.locate(directions);
+		
+		assertThat(located.get(RedLightCamera.class).size(), equalTo(1));
+		assertThat(located.get(SpeedCamera.class).size(), equalTo(3));
+		
+		Set<RedLightCamera> rlCameras = located.get(RedLightCamera.class);
+		Iterator<RedLightCamera> rlIter = rlCameras.iterator();
+		assertThat(rlIter.next(), equalTo(
+				new RedLightCamera(new HashSet<>(Arrays.asList("Columbus", "Illinois")),
+						new Location(41.891002, -87.620224),
+						EnumSet.of(Approach.NORTHBOUND, Approach.SOUTHBOUND))));
+
+		NavigableSet<SpeedCamera> spCameras = located.get(SpeedCamera.class, Cameras.Sorted.BY_LONGITUDE);
+		Iterator<SpeedCamera> spIter = spCameras.iterator();
+		assertThat(spIter.next(), equalTo(
+				new SpeedCamera("450 N Columbus Dr", new Location(41.890122352505166, -87.62041639513696),
+						EnumSet.of(Approach.SOUTHBOUND))));
+		assertThat(spIter.next(), equalTo(
+				new SpeedCamera("449 N Columbus Dr", new Location(41.89003775504898, -87.62017903427099),
+						EnumSet.of(Approach.NORTHBOUND))));
+		assertThat(spIter.next(), equalTo(
+				new SpeedCamera("319 E Illinois St", new Location(41.89091009572781, -87.61934752145963),
+						EnumSet.of(Approach.EASTBOUND))));
 	}
 }
