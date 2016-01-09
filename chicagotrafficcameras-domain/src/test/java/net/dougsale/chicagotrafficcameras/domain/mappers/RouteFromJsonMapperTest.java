e4008af1115c6e8e3c7a55b8bb807b98f687fffe
Doug Sale <dougsale@gmail.com>
2016-01-09 04:35:21 +0000

    used RouteBuilder in test

--- chicagotrafficcameras-domain/src/test/java/net/dougsale/chicagotrafficcameras/domain/mappers/RouteFromJsonMapperTest.java
+++ chicagotrafficcameras-domain/src/test/java/net/dougsale/chicagotrafficcameras/domain/mappers/RouteFromJsonMapperTest.java
@@ -7,14 +7,10 @@ package net.dougsale.chicagotrafficcameras.domain.mappers;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.junit.Test;
 
-import net.dougsale.chicagotrafficcameras.domain.Location;
 import net.dougsale.chicagotrafficcameras.domain.Route;
-import net.dougsale.chicagotrafficcameras.domain.Route.Step;
+import net.dougsale.chicagotrafficcameras.domain.builders.RouteBuilder;
 
 public class RouteFromJsonMapperTest {
 
@@ -24,16 +20,14 @@ public class RouteFromJsonMapperTest {
 	public void testMap() throws MapperException {
 		String json = "{\"startAddress\":\"716 S Central Park Ave, Chicago, IL 60624, USA\",\"endAddress\":\"401 N Central Park Ave, Chicago, IL 60624, USA\",\"steps\":[{\"instructions\":\"Head <b>north</b> on <b>S Central Park Ave</b> toward <b>W Flournoy St</b>\",\"start\":{\"latitude\":41.8721333,\"longitude\":-87.71559030000003},\"end\":{\"latitude\":41.8848491,\"longitude\":-87.7168211}},{\"instructions\":\"Continue onto <b>N Conservatory Dr</b>\",\"start\":{\"latitude\":41.8848491,\"longitude\":-87.7168211},\"end\":{\"latitude\":41.887812,\"longitude\":-87.71656229999996}},{\"instructions\":\"Continue onto <b>N Central Park Ave</b>\",\"start\":{\"latitude\":41.887812,\"longitude\":-87.71656229999996},\"end\":{\"latitude\":41.8884337,\"longitude\":-87.71657859999999}},{\"instructions\":\"Turn <b>right</b> to stay on <b>N Central Park Ave</b><div style=\\\"font-size:0.9em\\\">Destination will be on the right</div>\",\"start\":{\"latitude\":41.8884337,\"longitude\":-87.71657859999999},\"end\":{\"latitude\":41.8885286,\"longitude\":-87.7162669}}]}";
 		
-		String startAddress = "716 S Central Park Ave, Chicago, IL 60624, USA";
-		String endAddress = "401 N Central Park Ave, Chicago, IL 60624, USA";
-		List<Step> steps = Arrays.asList(
-			new Step("Head <b>north</b> on <b>S Central Park Ave</b> toward <b>W Flournoy St</b>", new Location(41.8721333, -87.71559030000003), new Location(41.8848491, -87.7168211)),
-			new Step("Continue onto <b>N Conservatory Dr</b>", new Location(41.8848491, -87.7168211), new Location(41.887812, -87.71656229999996)),
-			new Step("Continue onto <b>N Central Park Ave</b>", new Location(41.887812, -87.71656229999996), new Location(41.8884337, -87.71657859999999)),
-			new Step("Turn <b>right</b> to stay on <b>N Central Park Ave</b><div style=\"font-size:0.9em\">Destination will be on the right</div>", new Location(41.8884337, -87.71657859999999), new Location(41.8885286, -87.7162669))
-		);
-		
-		Route route = new Route(startAddress, endAddress, steps);
+		Route route = new RouteBuilder()
+				.withStartAddress("716 S Central Park Ave, Chicago, IL 60624, USA")
+				.withEndAddress("401 N Central Park Ave, Chicago, IL 60624, USA")
+				.withStep("Head <b>north</b> on <b>S Central Park Ave</b> toward <b>W Flournoy St</b>", 41.8721333, -87.71559030000003, 41.8848491, -87.7168211)
+				.withStep("Continue onto <b>N Conservatory Dr</b>", 41.8848491, -87.7168211, 41.887812, -87.71656229999996)
+				.withStep("Continue onto <b>N Central Park Ave</b>", 41.887812, -87.71656229999996, 41.8884337, -87.71657859999999)
+				.withStep("Turn <b>right</b> to stay on <b>N Central Park Ave</b><div style=\"font-size:0.9em\">Destination will be on the right</div>", 41.8884337, -87.71657859999999, 41.8885286, -87.7162669)
+				.build();
 		
 		assertThat(mapper.map(json), equalTo(route));
 	}
